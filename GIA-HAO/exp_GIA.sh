e83a735c92cf7ad3b7d749d3e2b6147d1b85d323
Leirunlin <lei_runlin@163.com>
2022-10-08 17:40:46 +0800

    GIA

---
+++ GIA-HAO/exp_GIA.sh
@@ -0,0 +1,10 @@
+for dataset in 'grb-cora' 'grb-citeseer'
+do
+for atk in gia seqgia agia
+do
+for alpha in 0.1 0.2 0.5 0.9
+do
+python gnn_misg.py --dataset $dataset  --inductive --eval_robo --model 'evennet' --eval_robo_blk --use_ln 1 --K 2 --eval_attack $atk --alpha $alpha
+done
+done
+done
